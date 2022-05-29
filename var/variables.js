0c272ef6bed523ac9f7efce7ac47cc68bfd2912d
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-28 23:05:42 -0300

    Iniciando meu projeto para ensinar javaS

---
+++ var/variables.js
@@ -0,0 +1,36 @@
+// Neste arquivo, irei documentar o uso de variáveis do JS 
+
+/* O que é são variáveis? 
+  RESPOSTA: é um objeto capaz de reter e representar um valor ou expressão
+*/
+
+ /* Identificadores: 
+  °Podem começar com($, letras ou _),
+  °Não iniciar com números,
+  °Não se pode usar espaçamento,
+  °Não se pode usar palavras reservadas,
+  OBS: CaseSensitive(A <> a) exemplo: CEU é diferente de ceu(variáveis diferentes)
+ */
+
+  var nome = "Teste";
+  // nesta variável acima, estamos criando do tipo var(será explicado abaixo) do tipo STRING, com atribuição da palavra teste
+  // Atribuindo outro valor para a mesma variável:
+  nome = "Teste01";
+
+  /* 
+   Data types{ (typeof)
+       number(infinity, NaN),
+       string
+       boolean
+       null
+       undefined
+       object
+       function
+   }
+  */
+
+ //Usando TypeOF:
+ function teste() {
+    const teste = 3.50;
+    typeof teste;
+ }  
