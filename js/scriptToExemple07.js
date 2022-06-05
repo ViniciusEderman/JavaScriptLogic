6986cb40d5523da3ec8740d97b3c0e7936c5d1ec
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-05 17:19:15 -0300

    all

--- js/scriptToExemple07.js
+++ js/scriptToExemple07.js
@@ -14,7 +14,6 @@ function Num(n) {
 }
 
 function Lista(n, l) {
-
     if(l.indexOf(Number(n)) != -1) {
         return true;
     }
@@ -26,7 +25,6 @@ function Lista(n, l) {
 function add() {
 
     if(Num(num.value) && !Lista(num.value, vector)) {
-
         vector.push(Number(num.value));
 
         let item = document.createElement('option');
@@ -47,14 +45,32 @@ function add() {
 function notFinish() {
 
     if(vector.length == 0) {
-
         window.alert('Adicione pelo menos 1 valor');
     }
     else {
-
         let total = vector.length;
+        let maior = vector[0];
+        let menor = vector[0];
+        let soma  = 0;
+        let media = 0;
+
+        for(let i in vector) {
+            soma += vector[i];
+
+            if(vector[i] > maior) {
+                maior = vector[i];
+            }
+            if(vector[i] < menor) {
+                menor = vector[i];
+            }
+        }
+        media += soma / total;
 
         resultado.innerHTML = '';
         resultado.innerHTML += `<p>No total temos: ${total} números já cadastrados</p>`;
+        resultado.innerHTML += `<p>O maior valor é: ${maior}</p>`;
+        resultado.innerHTML += `<p>O menor valor é: ${menor}</p>`;
+        resultado.innerHTML += `<p>A soma de todos os valores é: ${soma}</p>`;
+        resultado.innerHTML += `<p>A média de todos os valores é: ${media}</p>`;
     }
 }
