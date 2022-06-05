660fbadee3a44ab9ace409ecc8d4ac5b16de405d
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-05 16:06:38 -0300

    Update logica do js

--- js/scriptToExemple07.js
+++ js/scriptToExemple07.js
@@ -1,3 +1,32 @@
+let num = document.getElementById('num'); 
+let lista = document.getElementById('lista');
+let resultado = document.getElementById('resultado');
+let vector = [];
+
+function Num(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function Lista(n, l) {
+    if(lista.indexOf(Number(n)) != -1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 function add() {
-    
+
+    if(Num(num.value) && !Lista(num.value, vector)) {
+
+    }
+    else {
+        window.alert('Valor inválido');
+    }
 }
