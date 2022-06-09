2d38649641ab637558ffa68c529dd7c1504f2f40
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-09 10:00:50 -0300

    Finalizando meu serv express

--- Servidor-Express/index.js
+++ Servidor-Express/index.js
@@ -1,1 +1,18 @@
 
+const express = require('express');
+const app = express();
+const port = 8000; // porta de acesso ao serv 
+
+//rota do serv:
+app.get('/' , (req, res) => {
+    res.send('Rota funcionando');
+});
+
+//teste de outra rota:
+app.get('/olamundo' , (req, res) => {
+    res.send('<h1>Teste nova rota</h1>')
+});
+
+app.listen(port , () => {  //iniciando o servidor, criação de uma arrow function
+    console.log("Servidor funcionando");
+});
