0ac348a31afe854e63fbcc3edb1418d60c5aa94b
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-10 10:27:57 -0300

    Finalizando um servidor com express e nodemailer

--- Servidor-Express/index.js
+++ Servidor-Express/index.js
@@ -1,10 +1,23 @@
 
 const express = require('express');
+const nodemailer  = require('nodemailer');
 const app = express();
 const port = 8000; // porta de acesso ao serv 
 
 //rota do serv:
 app.get('/' , (req, res) => {
+    email.sendMail({
+
+        from: 'vinicius ederman <vinicius-edermanmc@hotmail.com>',
+        to: 'layssa.lms0@gmail.com',
+        subject: 'Hi, teste do meu serv',
+        text: 'Te amo gatona'
+    
+    }).then(message => {
+        console.log(message);
+    }).catch(err => {
+        console.log(err);
+    }); 
     res.send('Rota funcionando');
 });
 
@@ -16,3 +29,30 @@ app.get('/olamundo' , (req, res) => {
 app.listen(port , () => {  //iniciando o servidor, criação de uma arrow function
     console.log("Servidor funcionando");
 });
+
+let email = nodemailer.createTransport({
+
+    host: 'smtp-mail.outlook.com', 
+    secureConnection: false, 
+    port: 587, 
+    auth: {
+        user: 'vinicius-edermanmc@hotmail.com',
+        pass: '6267257Hh'
+    },
+    tls:{
+        ciphers:'SSLv3'
+    }
+}); 
+
+/*email.sendMail({
+
+    from: 'vinicius ederman <vinicius-edermanmc@hotmail.com>',
+    to: 'layssa.lms0@gmail.com',
+    subject: 'Hi, teste do meu serv',
+    text: 'Te amo gatona'
+
+}).then(message => {
+    console.log(message);
+}).catch(err => {
+    console.log(err);
+}); */
