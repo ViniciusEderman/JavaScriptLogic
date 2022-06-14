e12ccc55bd50cca73aa75a3a63185710cb5d5d34
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-14 13:05:14 -0300

    Update TesteCourse4.js

--- js/TesteCourse4.js
+++ js/TesteCourse4.js
@@ -1,3 +1,4 @@
+//exerício 01
 let computador = {
     processador: 1,
     placaDeVideo: 1,
@@ -7,3 +8,14 @@ let computador = {
 computador.fonte = 'TermalTake';
 
 console.log(computador);
+
+// exercício 02
+let arrayX = ["joao", "carlos", "maiza", "felipe", "luan"];
+    if(arrayX[0] == 'vinicius') {
+        console.log("Seu nome existe no meu array");
+    }
+    else{
+        console.log("Não existe");
+    }
+
+// exercício 02 forma 0.2 de fazer
