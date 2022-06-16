a961a223829509dd1ab5dce823bc6278d3e7c929
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-16 14:35:18 -0300

    novo exercício

--- js/TesteCourse7.js
+++ js/TesteCourse7.js
@@ -1,12 +1,28 @@
 class Car {
-    constructor(marca, cor, gasolina, restoGasolina) {
+    constructor(marca, cor, gasolinaTotal, consumo) {
         this.marca = marca;
         this.cor = cor;
-        this.gasolina = gasolina;
-        this.restoGasolina  = restoGasolina;
+        this.gasolinaTotal = gasolinaTotal;
+        this.consumo  = consumo;
+    }
+    dirigir(km) {
+        let consumoLitro = km / this.consumo;
+        this.gasolinaTotal = this.gasolinaTotal - consumoLitro;
+    }
+    abastecer(litro) {
+        this.gasolinaTotal = this.gasolinaTotal + litro;
     }
 }
 
-let car = new Car("GOL", "Blue", false, 0);
+let car = new Car("GOL", "Blue", 100, 14);
+console.log(car);
 
+console.log("---------------------------");
+
+car.dirigir(100);
+console.log(car);
+
+console.log("---------------------------");
+
+car.abastecer(4);
 console.log(car);
