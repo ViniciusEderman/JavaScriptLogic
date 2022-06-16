ed873190f4021573958e8c887a956b82ed76bcef
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-16 15:02:51 -0300

    exercício não finalizado

--- js/TesteCourse7.js
+++ js/TesteCourse7.js
@@ -1,4 +1,4 @@
-class Car {
+/*class Car {
     constructor(marca, cor, gasolinaTotal, consumo) {
         this.marca = marca;
         this.cor = cor;
@@ -25,4 +25,50 @@ console.log(car);
 console.log("---------------------------");
 
 car.abastecer(4);
-console.log(car);
+console.log(car); */
+
+// Novo exercício:
+class Conta {
+    constructor(saldoContaC, saldoContaP, juros) {
+        this.saldoContaC = saldoContaC;
+        this.saldoContaP = saldoContaP;
+        this.juros = juros;
+    }
+    depositar(valor) {
+        this.saldoContaC = this.saldoContaC + valor;
+    }
+    saque(valor) {
+        this.saldoContaC = this.saldoContaC - valor;
+    }
+    transfereciaCParaP(valor) {
+        this.saldoContaC -= valor;
+        this.saldoContaP += valor;
+    }
+    transfereciaPParaC(valor) {
+        this.saldoContaP -= valor;
+        this.saldoContaC += valor;
+    }
+    aplicarJuros(juros) {
+        this.juros = juros;
+        let valorJuros = (this.saldoContaC * juros) / 100;
+        this.saldoContaC = this.saldoContaC - valorJuros;
+    }
+}
+
+let conta = new Conta(695.80, 2000.00, 0);
+console.log(conta);
+
+conta.depositar(200);
+console.log(conta);
+
+conta.saque(100);
+console.log(conta);
+
+conta.transfereciaCParaP(100);
+console.log(conta);
+
+conta.transfereciaPParaC(1000);
+console.log(conta);
+
+conta.aplicarJuros(2.5);
+console.log(conta);
