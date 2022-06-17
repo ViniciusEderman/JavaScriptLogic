c55d169fc303da109f524225b232cd2f89747bfd
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-17 09:32:28 -0300

    prompt + throw error

--- js/TesteCourse8.js
+++ js/TesteCourse8.js
@@ -14,4 +14,18 @@ checarEntradaDados(33);
 
 //Exemplo usando o DOM do js:
 let entrada = prompt("Digite um número: ");
-checarEntradaDados(entrada);
+checarEntradaDados(entrada); 
+
+//Exceptions:
+function teste(nome) {
+    if(typeof nome != 'string') {
+        throw new Error("Nome precisa ser uma string");
+    }
+    else {
+        console.log(`Olá, tudo bem? O seu nom é: ${nome}`);
+    }
+}
+
+teste("Vinicius Ederman");
+teste(185);
+
