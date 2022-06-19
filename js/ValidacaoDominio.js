d8747fa00949f17c5af22555389b0d542f341164
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-19 18:12:32 -0300

    novo exercíco

--- js/ValidacaoDominio.js
+++ js/ValidacaoDominio.js
@@ -7,3 +7,8 @@ console.log(validarDominio.test("www.vinicius.com"));
 
 console.log(validarDominio.test("www.vinicius.com.br"));
 
+// Regex validação de email:
+let validarEmail = /\w+@\w+\.\w+/;
+console.log(validarEmail.test("viniciusederman@"));
+console.log(validarEmail.test("viniciusedermanmc@hotmail.com"));
+console.log(validarEmail.test("string"));
