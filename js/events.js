c799201a1d5b59c8b39f63af521d4d806fc62e3b
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-23 10:07:05 -0300

    evento com js

---
+++ js/events.js
@@ -0,0 +1,10 @@
+
+let botao = window.document.querySelector("#botao");
+ // Estou apenas pegando algum elemento no html e selecionando ele para o js
+
+botao.addEventListener("click", function() {
+    console.log("Usuário clicou");
+});
+ /* Neste ponto eu estou atribuinto ao botao -> um evento(do tipo click);
+  Sendo assim printado que o usuário clicou */
+
