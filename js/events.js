560a93b645960573a50678e1686bd8cbe29c0e0f
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-23 10:18:14 -0300

    Remove Event + function com event

--- js/events.js
+++ js/events.js
@@ -14,3 +14,15 @@ let btn = document.querySelector("#btn");
 btn.addEventListener("click", () => {
     console.log("O usuário clicou aqui!");
 });
+
+// Como remover um event:
+function mensagem() {
+    console.log("O usuário clicou");
+}
+
+btn.addEventListener("click", mensagem); // passando a function como event
+
+setTimeout(function() {
+    btn.removeEventListener("click", mensagem); // removendo o evento
+},3000); // removendo o evento depois de 3segundos! 
+
