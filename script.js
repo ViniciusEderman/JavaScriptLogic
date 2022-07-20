c08c221f25d9d7df44ff3599d657697a156a3daa
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-07-20 20:09:40 -0300

    Função de teste

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Script de teste baseado nos seguintes fatores:
+    se o usuário digitar sim ou s ele entra no if
+    e se ele digitar n ou não entra no else */
+
+function LoveIsLove() {
+
+    trueLove;
+    falseLove = "I dont like u";
+
+    if(trueLove = 1) {
+        for(i = 0; i < trueLove; i++) {
+            console.log("I love u");
+        }
+    }
+    else {
+        console.log(falseLove);
+    }
+}
