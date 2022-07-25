238356d4c8eeafbad3d90c0533ec84fd133b64ef
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-07-25 18:05:18 -0300

    problema ao imprimir o array

---
+++ js/ArrayGame.js
@@ -0,0 +1,12 @@
+function ArrayGame() {
+
+    let array = [1, 2, 3, 18, 60, 80 ,99];
+        
+     // console.log(array); 
+
+    for(let i = 0; i < array.length; i++) {
+        console.log(i[4]);
+    }
+}
+
+ArrayGame();
