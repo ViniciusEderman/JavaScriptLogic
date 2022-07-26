ba22ec2f52163c3c623dae0150ead0ee7322a22f
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-07-26 08:03:47 -0300

    Agora funcionando

--- js/ArrayGame.js
+++ js/ArrayGame.js
@@ -2,12 +2,10 @@ function ArrayGame() {
 
     let array = [1, 2, 3, 18, 60, 80 ,99];
         
-     // console.log(array); 
-
-    for(let i = 0; i <= array.length; i++) {
-        // há forma de imprimir todo o conteúdo: 0 = 1, 1 = 2...
+    for(let i = 0; i < array.length; i++) {
+        console.log(array[i]);
     }
-    console.log(array[4]);
+    
 }
 
 ArrayGame();
