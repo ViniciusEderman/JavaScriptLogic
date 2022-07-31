3fc30538eb147c3e69b294798a84811f19489528
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-07-31 16:11:06 -0300

    Question exercise done

---
+++ js/Exercise00.js
@@ -0,0 +1,14 @@
+
+const firstValue = 10;
+const secondValue = 1515;
+let Fvalue = 0;
+let Svalue = 0;
+
+if(firstValue > 9) {
+    Fvalue = firstValue * 0.70;
+    console.log(Fvalue);
+}
+if(secondValue > 1514) {
+    Svalue = secondValue * 0.75;
+    console.log(Svalue);
+}
