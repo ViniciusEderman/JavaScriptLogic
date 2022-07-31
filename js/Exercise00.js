5607a03432bcaaa067a2b285e6bae2571041cbde
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-07-31 16:23:09 -0300

    question 02, done

--- js/Exercise00.js
+++ js/Exercise00.js
@@ -1,5 +1,6 @@
+// first question:
 
-const firstValue = 10;
+/*const firstValue = 10;
 const secondValue = 1515;
 let Fvalue = 0;
 let Svalue = 0;
@@ -11,4 +12,17 @@ if(firstValue > 9) {
 if(secondValue > 1514) {
     Svalue = secondValue * 0.75;
     console.log(Svalue);
-}
+} */
+
+//second question:
+/*const fixSalary = 1600.00;
+let salles = 15;
+let sallerPricy = 1500.00;
+const comission = 0.85 * (1500.00 * salles);
+let finallyValue = 0;
+
+if(salles > 14) {
+    finallyValue = comission + fixSalary;
+    console.log(`Your salles is: ${salles},\n and your salary is: ${fixSalary}.\n All things: ${finallyValue}`)
+} */
+
