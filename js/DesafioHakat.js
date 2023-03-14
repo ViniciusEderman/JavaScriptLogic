60902ea701096b545f364628acadc31509cc5f86
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-03-14 20:12:30 -0300

    criando o cabeçalho da questão

--- js/DesafioHakat.js
+++ js/DesafioHakat.js
@@ -1,3 +1,6 @@
+ /* 
+ Implemente uma função que recebe 2 números como parâmetros e retorna um array, que retorna os números consecutivos ao 1° parâmetro até o valor em que, ao somar todos os números no array, temos o valor passado no 2 parâmetro.
+ */
 
 function returnArray(firstNumber, secondNumber) {
 
