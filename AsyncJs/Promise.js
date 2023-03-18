9a1f3957aa1a46f4e116236f5d636521983bd945
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-03-18 08:04:51 -0300

    mostrando as funções na prática

--- AsyncJs/Promise.js
+++ AsyncJs/Promise.js
@@ -26,3 +26,15 @@ function enviarEmail(corpo, para){
         },4000)
     });
 }
+
+console.log("Inicio!");
+pegarId().then((id) => {
+    buscarEmailNoBanco(id).then((email) => { 
+        enviarEmail("Olá, como vai?",email).then(() => {
+            console.log("Email enviado, para o usuário com id: " + id)
+        }).catch(err => {
+            console.log(err);
+        })
+    })
+})
+console.log("FIM");
