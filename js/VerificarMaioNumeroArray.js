c66795fe79b7f7ebd1444aed0d654535b322a102
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-03-21 14:07:02 -0300

    verificando o maior numero do array com math.max

--- js/VerificarMaioNumeroArray.js
+++ js/VerificarMaioNumeroArray.js
@@ -2,13 +2,11 @@
 const array = [55, 20, 50, 150, 200, 155, 99, 2, 0, -56, 15, 33];
 
 let maiorNumeroAte = array[0];
-//console.log(maiorNumeroAte); // [0] = 55
 
 if(array.length >= 0) {
     for(i = 1; i < array.length; i++) {
         if(array[i] > maiorNumeroAte) {
             maiorNumeroAte = array[i]; // posição i seria o maior valor passado por todo o array, utilizado pelo length
-            //console.log(maiorNumeroAte);
         }  
     }
 } 
@@ -16,4 +14,9 @@ else {
     console.log("Seu array não tem números para verificar");
 }
 
-console.log(maiorNumeroAte);
+//console.log(maiorNumeroAte);
+
+// fazendo a mesma questão usando o operador math.max:
+
+const max = Math.max.apply(null, array); // << usando o metódo math.max e passando o array como parâmetro ele verifica o maior nm
+console.log(max);
