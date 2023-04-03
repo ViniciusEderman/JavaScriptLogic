1193fbf21385ebcd989bb1c92a98aca07d31ed7f
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-04-03 16:36:26 -0300

    acessando os atributos da classe

--- Poo/FirstClass.js
+++ Poo/FirstClass.js
@@ -26,3 +26,13 @@ class Movie {
         console.log("closing...");
     }
 }
+
+// accessing a class
+const theGodFather = new Movie();
+
+theGodFather.title = 'The God Father Part 1';
+theGodFather.year = 1972;
+theGodFather.movieGenre = 'police thriller';
+theGodFather.actors = ['Al Pacino', 'Robert De Niro', 'Marlon Brando'];
+console.log(theGodFather.title);
+console.log(theGodFather.actors);
