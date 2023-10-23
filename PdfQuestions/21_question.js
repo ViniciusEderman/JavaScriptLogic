cc9e2b82e71025c3048684cb27651518393f0d4d
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2023-10-23 18:57:05 -0300

    finalizando questão 21

--- PdfQuestions/21_question.js
+++ PdfQuestions/21_question.js
@@ -3,7 +3,7 @@ Ler a hora de início e a hora de fim de um jogo de Xadrez (considere apenas hor
 minutos) e calcule a duração do jogo em horas, sabendo-se que o tempo máximo de duração do jogo é
 de 24 horas e que o jogo pode iniciar em um dia e terminar no dia seguinte. 
 */
-let inicio = 15;
+let inicio = 18;
 let fim = 4;
 let tempoTotal;
 
@@ -11,5 +11,6 @@ if((inicio <= 0 || inicio >= 24) && (fim <= 0 || fim >= 24)) {
     console.log('Inicio e Fim não podem ser maior igual a 24 ou menor igual a 0');
 }
 else {
-    
+    tempoTotal = (24 - inicio) + fim;
+    console.log(tempoTotal);
 }
